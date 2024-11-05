66e7dbac9680a667bbbc917bcf295d324c0aba63
Leonardo Noriega <leonoriega100@gmail.com>
2024-11-05 13:06:53 -0600

    feat: CRUD of users finished with ORM validations

---
+++ app/controllers/userController.js
@@ -0,0 +1,130 @@
+import User from '../models/User.js';
+import bcrypt from 'bcrypt';
+
+const createUser = async (req, res) => {
+  try {
+    const { password, ...restUserData } = req.body;
+    if (password.length < 8 || password.length > 100) {
+      return res.status(400).json({ error: 'La contraseña debe tener al menos 8 caracteres' });
+    }
+    const existingUser = await User.findOne({ where: { email: restUserData.email } });
+    if (existingUser) {
+      return res.status(400).json({ error: 'El email ya está registrado' });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      password: hashedPassword,
+      ...restUserData
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    console.error("Error al crear el usuario:", error);
+    if (error.name === 'SequelizeValidationError') {
+      const validationErrors = {};
+      error.errors.forEach(err => {
+        validationErrors[err.path] = err.message;
+      });
+      return res.status(400).json({ errors: validationErrors });
+    } else {
+      return res.status(500).json({ error: "Error al crear el usuario" });
+    }
+  };
+};
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error al obtener los usuarios:", error);
+    res.status(500).json({ error: "Error al obtener los usuarios" });
+  }
+};
+
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ error: "Usuario no encontrado" });
+    }
+  } catch (error) {
+    console.error("Error al obtener el usuario:", error);
+    res.status(500).json({ error: "Error al obtener el usuario" });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (user) {
+      const { password, ...restUserData } = req.body;
+      await user.update(restUserData);
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ error: "Usuario no encontrado" });
+    }
+  } catch (error) {
+    console.error("Error al actualizar el usuario:", error);
+    if (error.name === 'SequelizeValidationError') {
+      const validationErrors = {};
+      error.errors.forEach(err => {
+        validationErrors[err.path] = err.message;
+      });
+      return res.status(400).json({ errors: validationErrors });
+    } else {
+      res.status(500).json({ error: "Error al actualizar el usuario" });
+    }
+  }
+}
+
+const updatePassword = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+    const { currentPassword, newPassword } = req.body;
+    const passwordMatch = await bcrypt.compare(currentPassword, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Contraseña actual incorrecta" });
+    }
+    if (newPassword.length < 8 || newPassword.length > 100) {
+      return res.status(400).json({ errors: { newPassword: "La nueva contraseña debe tener entre 8 y 100 caracteres" } });
+    }
+    const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+    await user.update({ password: hashedNewPassword });
+    res.status(200).json({ message: "Contraseña actualizada correctamente" });
+  } catch (error) {
+    console.error("Error al actualizar la contraseña:", error);
+    res.status(500).json({ error: "Error al actualizar la contraseña" });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (user) {
+      await user.destroy();
+      res.status(200).json({
+        message: "Usuario eliminado",
+        user: user
+      });
+    } else {
+      res.status(404).json({ error: "Usuario no encontrado" });
+    }
+  } catch (error) {
+    console.error("Error al eliminar el usuario:", error);
+    res.status(500).json({ error: "Error al eliminar el usuario" });
+  }
+}
+
+export {
+  createUser,
+  getUsers,
+  getUserById,
+  updateUser,
+  updatePassword,
+  deleteUser
+}
--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import sequelize from './app/config/db.js'
+import userRoutes from './app/routes/userRoutes.js'
+
+dotenv.config()
 
 const app = express()
 const PORT = process.env.NODE_DOCKER_PORT
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
+
+app.use('/toystore', userRoutes)
+
+async function start() {
+  try {
+    await sequelize.sync({ alter: true})
+    console.log('Conexión a la base de datos establecida correctamente.')
+  } catch (error) {
+    console.error('Error al conectar a la base de datos', error)
+  }
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${process.env.NODE_LOCAL_PORT}`)
   console.log('a')
 })
+
+start()
