5c7123de385d4c32b567c2b364c331b143a27793
Leo-Noriega <leonoriega100@gmail.com>
2024-12-02 23:42:37 -0600

    feat: functional cart functions

--- app/routes/productRoutes.js
+++ app/routes/productRoutes.js
@@ -10,6 +10,7 @@ router.get('/products/:id', getProductById);
 router.put('/products/:id', upload.array('images', 5), updateProduct);
 router.delete('/products/:id', deleteProduct);
 router.get('/products/category/:categoryId', getProductsByCategory);
+router.get('/products/total-stock-by-category', getTotalStockByCategory); // Ruta para obtener el stock total por categoría
 router.put('/products/:id/stock/:quantity', updateStock);
 
 
